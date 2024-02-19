410ccb6fe0e5416db44a596f596469c398932afe
Lucy-Dev85 <belenduarte37@hotmail.com>
2024-02-19 20:10:13 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Dados';
+ dadoIzquierda = '../assets/img/dice1.png';
+ dadoDerecha = '../assets/img/dice4.png';
+ numero1 = 1;
+ numero2 = 2 ; 
+
+ tirarDados(): void {
+  this.numero1 =Math.round( Math.random() *5) +1;
+  this.numero2 =Math.round( Math.random() *5) +1;
+  this.dadoIzquierda = '../assets/img/dice' +  this.numero1 + '.png';
+  this.dadoDerecha = '../assets/img/dice' +  this.numero2 + '.png';
+
+ }
 }
